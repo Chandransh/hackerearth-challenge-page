ebf7c9027acac1c5d005f692448a24c250f777f3
chandransh <chandransh.s@gmail.com>
2017-01-14 20:08:09 +0530

    Card made dynamic and design improved

--- src/js/components/ChallengeCard/ChallengeCard.js
+++ src/js/components/ChallengeCard/ChallengeCard.js
@@ -8,23 +8,23 @@ import './ChallengeCard.css';
 
 const ChallengeCard = ({thumbnail, title, url, challengeType, startTimestamp}) => (
   <div className="challenge-card">
+    <a href={url}></a>
     <div className="thumbnail full-width float-left">
       <img className="full-width float-left"
-           src="https://hackerearth-media.global.ssl.fastly.net/media/hackathon/flockathon/images/f718e452-b-c66888de-9-listing%20%281%29.png" alt="title"/>
+           src={thumbnail} alt={title}/>
     </div>
-    <p className="challenge-type">
-      <span>&#9968; </span>Hackathon
-    </p>
-    <a href="/">
-      {/*<h4>{(ngoName.length>25) ? ngoName.substring(0,25)+'...' : ngoName}</h4>*/}
-    </a>
-    <h3 className="title text-ellipsis"
-        title="title">Battle Of Bots #8Battle Of Bots #8</h3>
-    <p className="text-secondary text-uppercase">
-      <small>Starts on</small>
-    </p>
-    <p>Jan 14, 11 AM IST</p>
-    <a href="#" className="btn btn-outline">View Details</a>
+    <div className="card-content">
+      {(challengeType.toLowerCase()==="monthly challenges") ?
+        <p className="challenge-type" style={{backgroundColor:"#aeff5a"}}>&#9873; Monthly Challenge</p>
+        : <p className="challenge-type">&#9968; Hiring Challenge</p>}
+      <h3 className="title text-ellipsis"
+          title={title}>{title}</h3>
+      <p className="text-secondary text-uppercase">
+        <small>Starts on</small>
+      </p>
+      <p>{startTimestamp}</p>
+      <a href={url} className="btn btn-outline full-width">View Details</a>
+    </div>
   </div>
 );
 
