02e614e15a5a5b9dc339ff57e7549994bc0b27d0
chandransh <chandransh.s@gmail.com>
2017-01-15 00:24:39 +0530

    First build

--- src/js/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-
-import getUpcomingChallenges from './getUpcomingChallenges';
-
-const rootReducer = combineReducers({
-  getUpcomingChallenges
-});
-
-export default rootReducer;
